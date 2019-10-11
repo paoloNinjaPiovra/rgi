41be3549aaa6b7b98e365449f49b0a8d51d88a74
Fasoli Paolo <p.fasoli@gmail.com>
2019-10-11 17:02:59 +0200

    @transactional added

--- src/main/java/com/rgi/rgi/service/TaskService.java
+++ src/main/java/com/rgi/rgi/service/TaskService.java
@@ -9,10 +9,10 @@ import com.rgi.rgi.model.TaskList;
 
 public interface TaskService {
     TaskList getTaskList(String userSession) throws UserNotFoundException;
-    TaskDetail getTask(String userSession, String taskCode) throws UserNotFoundException;
+    TaskDetail getTask(String userSession, String taskCode) throws UserNotFoundException, TaskNotFoundException;
     Task deleteTask(String userSession, String taskCode) throws UserNotFoundException, TaskNotFoundException;
     Task saveOrUpdate(String userSession, TaskDetail newTask, String taskCode) throws UserNotFoundException;
     Task patch(String userSession, Task newTask, String taskCode) throws UserNotFoundException, TaskNotFoundException;
-    Task save(String userSession, TaskForm newTask) throws UserNotFoundException;
+    Task save(String userSession, TaskForm newTask) throws UserNotFoundException, TaskNotFoundException;
     Task closeTask(String userSession, String taskCode) throws UserNotFoundException, TaskNotFoundException;
 }
